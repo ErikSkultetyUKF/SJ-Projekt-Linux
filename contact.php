627376dec5af0e9ff83e25cc7361d3b5a7cd6ff9
ErikSkultetyUKF <erik.skultety@student.ukf.sk>
2024-04-23 17:38:20 +0200

    Drobné úpravy

--- contact.php
+++ contact.php
@@ -4,21 +4,18 @@
 
 <main>
 
-    <h1 class="cent">Kontakt</h1>
-    <p class="cent">Náš E-mail: info@projektlinux.org</p>
-    <p class="cent">Náš telefón: +421 123 456 789</p>
-    <p class="cent">Naša adresa: Tr. A. Hlinku 1, 949 01 Nitra</p>
+    <h1 class="cent">Máte otázky?</h1>
 
     <!-- Formulár -->
     <h2 class="cent">Napíšte nám:</h2>
 
     <form class="cent" action="thankyou.php">
 
-        <label for="email">E-mail:</label><br>
+        <label for="email">Váš e-mail:</label><br>
         <input type="email" class="email" name="email" placeholder="meno@priklad.org" value="" required><br>
     
-        <label for="note">Poznámka:</label><br>
-        <textarea class="note" name="note" maxlength="180" required></textarea><br><br>
+        <label for="message">Vaša správa:</label><br>
+        <textarea class="message" name="message" maxlength="250" required></textarea><br><br>
     
         <input type="checkbox" required>
         <label>Súhlasím so spracovaním osobných údajov</label><br><br>
--- partials/footer.php
+++ partials/footer.php
@@ -1,11 +1,21 @@
 <!-- Footer -->
 <!-- Inline štýl, footer ignoruje css-->
 <footer style="text-align: center; font-size: 1rem;">
-    <p>©2023-2024 Nadizajnoval - Erik Škultéty</p>
-    <p>Zdroje:</p>
-    <a href="https://www.linux.com/what-is-linux/" target="_blank">https://www.linux.com/what-is-linux/</a><br>
-    <a href="https://gs.statcounter.com/os-market-share/" target="_blank">https://gs.statcounter.com/os-market-share/</a><br>
-    <a href="https://www.atatus.com/blog/top-10-linux-distros/" target="_blank">https://www.atatus.com/blog/top-10-linux-distros/</a>
+    <div>
+        <h2>Kontaktujte nás</h2>
+        <p>info@projektlinux.org<br>
+        +421 123 456 789<br>
+        Tr. A. Hlinku 1, 949 01 Nitra</p>
+    </div>
+    <div>
+        <h2>Použité zdroje:</h2>
+        <a href="https://www.linux.com/what-is-linux/" target="_blank">https://www.linux.com/what-is-linux/</a><br>
+        <a href="https://gs.statcounter.com/os-market-share/" target="_blank">https://gs.statcounter.com/os-market-share/</a><br>
+        <a href="https://www.atatus.com/blog/top-10-linux-distros/" target="_blank">https://www.atatus.com/blog/top-10-linux-distros/</a>
+    </div>
+    <div>
+        <p>©2023-2024<br>Vytvoril a nadizajnoval - Erik Škultéty</p>
+    </div>
 </footer>
 
 <?php
